39906d835d318779f04a539b25b2dc3ed200db95
mvc1009 <mvc1009@gmail.com>
2020-04-22 14:10:58 +0200

    nmap: -sS mode

--- dbfinder_script.py
+++ dbfinder_script.py
@@ -1,11 +1,6 @@
 import os, sys
 import nmap
 
-f=open('list.txt', 'r')
-hosts = f.read().split()
-
-for i in hosts:
-    print(i)
 
 #
 # MOST COMMON DataBases known ports
@@ -30,19 +25,30 @@ for i in hosts:
 #	Solr:			7574,8983
 #
 
+def listHosts():
+	f=open('list.txt', 'r')
+	hosts = f.read().split()
+	list_of_hosts = ''
+	for i in hosts:
+    		list_of_hosts += i +',';
+	list_of_hosts = list_of_hosts[:-1]
+	print('List of Hosts to scan: ' + list_of_hosts)
+	return hosts
+
+
 db_knownports = '7210,3306,1521,1830,5432,1433,1434,8529,7000,7001,9042,5984,9200,9300,27017,27018,27019,28017,7473,7474,6379,8087,8089,8080,28015,29015,7574,8983'
 
 nmScan = nmap.PortScanner()
-nmScan.scan('127.0.0.1',db_knownports)
-for host in nmScan.all_hosts():
-	print('Host %s (%s)' % (host, nmScan[host].hostname()))
-	print('State : %s' % nmScan[host].state())
-	for proto in nmScan[host].all_protocols():
-		print('-----------')
-		print('Protocol : %s' % proto)
-		lport = nmScan[host][proto].keys()
+list_of_hosts = listHosts()
+for host in list_of_hosts:
+	nmScan.scan(host,db_knownports, arguments='-sS')
+	for host_up in nmScan.all_hosts():
+		print('[+] Host up %s\t(%s)' % (host, nmScan[host_up].hostname()))
+		for proto in nmScan[host_up].all_protocols():
+			lport = nmScan[host_up][proto].keys()
+			for port in lport:
+				print('\t[+] Port : %s\t%s' % (port , nmScan[host_up][proto][port]['name']))
 
-		for port in lport:
-			print('port : %s\tstate : %s' % (port , nmScan[host][proto][port]['state']))
-			print(nmScan[host][proto][port])
+
+
 
